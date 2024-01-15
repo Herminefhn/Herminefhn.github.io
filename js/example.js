a60ee021755fba87ac49425ce7abb719169c10ad
Herminefhn <143608009+Herminefhn@users.noreply.github.com>
2024-01-15 09:52:05 +0100

    Litt forandring

---
+++ js/example.js
@@ -0,0 +1,26 @@
+
+
+
+/** 
+
+let header = document.getElementById("header");
+header.innerText = "hei";
+header.style.color = "rgb(70, 155, 228)"
+
+
+let number1 = 30;
+let number2 = 20;
+let result;
+let text;
+
+result = number1 + number2;
+text = "resultatet blir: " + result;
+
+console.log(text);
+
+alert("Hallo!");
+
+let firstName = prompt("Kva heiter du?")
+header.innerText = "Velkommen,  " + firstName;
+
+*/
