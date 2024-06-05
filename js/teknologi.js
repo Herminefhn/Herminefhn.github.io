c2fe19f898e584738f707b42bdf5d4f66acf9da7
Herminefhn <143608009+Herminefhn@users.noreply.github.com>
2024-06-05 17:52:29 +0200

    gvsrbgg
    wrgesrg

--- js/teknologi.js
+++ js/teknologi.js
@@ -3,17 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentIndex = 0;
 
     function showNextImage() {
-        // Hide the current active image
         images[currentIndex].classList.remove('active');
-        // Calculate the index of the next image
         currentIndex = (currentIndex + 1) % images.length;
-        // Show the next image
         images[currentIndex].classList.add('active');
     }
 
     const nextButton = document.querySelector('.next-button');
     nextButton.addEventListener('click', showNextImage);
 
-    // Show the first image initially
     images[currentIndex].classList.add('active');
 });
